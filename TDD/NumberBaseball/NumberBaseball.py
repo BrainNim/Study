09508be634594fe6a9b78060fa7d234a3c42f19c
Lee <hhhsss0815@gmail.com>
2023-01-08 19:57:41 +0900

    TDD_1) 정답인 경우 3S - init 추가 -> 다양한 정답 테스트 가능

--- TDD/NumberBaseball/NumberBaseball.py
+++ TDD/NumberBaseball/NumberBaseball.py
@@ -1,6 +1,16 @@
 import random
 
 class Game:
+    def __init__(self, correct_answer):
+        if correct_answer:
+            self.correct_anser = correct_answer
+        else:
+            self.correct_anser = random.sample(['0','1','2','3','4','5','6','7','8','9'],3)
+            self.correct_anser = ''.join(self.correct_anser)
+
     def guess_checker(self, guess):
-        if guess == "369":
+        if guess == self.correct_anser:
             return "3S"
+
+        else:
+            return f"test용 print : {self.correct_anser}"
--- TDD/NumberBaseball/NumberBaseball_test.py
+++ TDD/NumberBaseball/NumberBaseball_test.py
@@ -1,10 +1,14 @@
 import unittest
+# from unittest.mock import Mock, MagicMock, call
 from NumberBaseball import Game
 
 class NumberBaseballTest(unittest.TestCase):
-    game = Game()
 
     def test_guess_correct_answer_then_3S(self):
-        guess = "369"
-        result = self.game.guess_checker(guess)
+        game = Game("369")
+        result = game.guess_checker("369")
         self.assertEqual("3S", result)
+
+        game1 = Game("106")
+        result1 = game1.guess_checker("106")
+        self.assertEqual("3S", result1)
